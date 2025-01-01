e351ce34581162c5bd68a80134eb87fd72899ec1
Nosulf <bulezar@gmail.com>
2025-01-01 19:56:17 -0300

    determinar si un número es par o impar

---
+++ guia-01/JavaIntroEj06.java
@@ -0,0 +1,10 @@
+package javaintroej06;
+
+//Crear un programa que dado un numero determine si es par o impar.
+
+public class JavaIntroEj06 {
+
+    public static void main(String[] args) {
+        
+    }
+}
